9c1dde2cf30e66f82708f56ba572cb7cb551e780
@12345Asmit <asmitshukla9794@gmail.com>
2026-07-08 19:59:12 +0530

    feat : tried to complete the question water taps of cf

---
+++ id2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+//water taps codeforces 40
+bool  msol(vector<int>a,vector<int>b,int res,int ){
+
+    //base case
+
+
+
+
+}
+
+int main(){
+
+    int n , t;
+    cin>>n>>t;
+    vector<int>a(n);
+    vector<int>t(n);
+
+    int e  = 0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        e += a[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>t[i];
+    }
+
+    int s=0;
+    while(s<=e){
+        int mid = s+ (e-s)/2;
+        if(msol(a,b,mid)){
+            s=mid+1;
+        }
+        else{
+            e = mid-1;
+        }
+    }
+    cout<<e<<endl;
+    return 0;
+
+}
